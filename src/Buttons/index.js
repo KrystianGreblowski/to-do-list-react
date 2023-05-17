35072b19c9cad648eeff5d0dfc7e81ab351c4c8d
Krystian Greblowski <krystian.greblowski@gmail.com>
2023-05-17 19:41:20 +0200

    initial commit

---
+++ src/Buttons/index.js
@@ -0,0 +1,19 @@
+import "./style.css";
+
+const Buttons = ({ tasks, hideDoneTasks }) => (
+    tasks.length > 0 && (
+        < div className="buttons" >
+            <button className={`buttons__singleButton`}>
+                {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
+            </button>
+            <button
+                className="buttons__singleButton"
+                disabled={tasks.every(({ done }) => done)}
+            >
+                Ukończ wszystkie
+            </button>
+        </div >
+    )
+);
+
+export default Buttons;
