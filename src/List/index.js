f0c3e3e6ad5c1f56b4787f62b308d3c64d75fa4b
Krystian Greblowski <krystian.greblowski@gmail.com>
2023-05-21 14:48:50 +0200

    indent changed to 2 spaces

--- src/List/index.js
+++ src/List/index.js
@@ -1,32 +1,32 @@
 import "./style.css";
 
 const List = ({ tasks, hideDoneTasks }) => (
-    <ul className="list">
-        {tasks.map(task => (
-            <li
-                key={task.id}
-                className={`list__item ${task.done && hideDoneTasks
-                    ? "list__item--hidden"
-                    : ""}`
-                }
-            >
-                <button className={`list__button ${task.done ? "list__button--done" : ""}`}>
-                    {task.done ? "✔" : ""}
-                </button>
+  <ul className="list">
+    {tasks.map(task => (
+      <li
+        key={task.id}
+        className={`list__item ${task.done && hideDoneTasks
+          ? "list__item--hidden"
+          : ""}`
+        }
+      >
+        <button className={`list__button ${task.done ? "list__button--done" : ""}`}>
+          {task.done ? "✔" : ""}
+        </button>
 
-                <span className={`list__content ${task.done
-                    ? "list__content--done"
-                    : ""}`
-                }>
-                    {task.content}
-                </span>
+        <span className={`list__content ${task.done
+          ? "list__content--done"
+          : ""}`
+        }>
+          {task.content}
+        </span>
 
-                <button className={`list__button list__button--remove`}>
-                    🗑
-                </button>
-            </li>
-        ))}
-    </ul >
+        <button className={`list__button list__button--remove`}>
+          🗑
+        </button>
+      </li>
+    ))}
+  </ul >
 );
 
 export default List; 
