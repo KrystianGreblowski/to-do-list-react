e9cb71fefd322a559b2357915c683d6da0a4b0e6
Krystian Greblowski <krystian.greblowski@gmail.com>
2023-05-21 21:15:22 +0200

    input value is not empty option

--- src/App.js
+++ src/App.js
@@ -43,14 +43,15 @@ function App() {
   };
 
   const addNewTask = (content) => {
-    setTasks((tasks) => [
-      ...tasks,
-      {
-        content,
-        done: false,
-        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
-      },
-    ]);
+    content &&
+      setTasks((tasks) => [
+        ...tasks,
+        {
+          content,
+          done: false,
+          id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+        },
+      ]);
   };
 
   return (
