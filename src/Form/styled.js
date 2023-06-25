c8bda7bfdfa6aa16952bc5e834cbb17429e7d616
Krystian Greblowski <krystian.greblowski@gmail.com>
2023-06-25 21:39:48 +0200

    styled components added to Form

--- src/Form/styled.js
+++ src/Form/styled.js
@@ -1,4 +1,6 @@
-.form {
+import styled from "styled-components";
+
+export const FormStyled = styled.form`
   display: grid;
   grid-template-columns: 1fr auto;
   grid-gap: 20px;
@@ -6,33 +8,27 @@
   box-shadow: 1px 1px 3px #ddd;
   padding: 20px;
   font-size: 14px;
-}
 
-@media (max-width: 767px) {
-  .form {
+  @media (max-width: 767px) {
     grid-template-columns: 1fr;
   }
-}
+`;
 
-.form__input {
+export const Input = styled.input`
   border: 1px solid #ccc;
   padding: 10px;
-}
+`;
 
-.form__button {
+export const Button = styled.button`
   background-color: hsl(180, 100%, 25%);
   border: none;
   padding: 10px;
   color: white;
   cursor: pointer;
   transition: 0.4s;
-}
 
-.form__button:hover {
-  background-color: hsl(180, 100%, 30%);
-  transform: scale(1.1);
-}
-
-.form__button:active {
-  background-color: hsl(180, 100%, 35%);
-}
+  &:hover {
+    background-color: hsl(180, 100%, 30%);
+    transform: scale(1.1);
+  }
+`;
