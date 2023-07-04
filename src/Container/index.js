a759e38b012aa85ac29b58c4c75a99dd7a11a8e2
Krystian Greblowski <krystian.greblowski@gmail.com>
2023-07-04 19:20:23 +0200

    code refactoring

--- src/Container/index.js
+++ src/Container/index.js
@@ -1,5 +1,13 @@
-import { Main } from "./styled";
+import styled from "styled-components";
 
-const Container = ({ children }) => <Main>{children}</Main>;
+export const StyledContainer = styled.main`
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+`;
+
+const Container = ({ children }) => (
+  <StyledContainer>{children}</StyledContainer>
+);
 
 export default Container;
