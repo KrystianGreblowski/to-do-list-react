0ac8547b93b24af46ce2399b49c4a3d782d8e236
Krystian Greblowski <krystian.greblowski@gmail.com>
2023-10-30 22:27:38 +0100

    unnecessary code removed

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -1,21 +1,11 @@
-import { useSelector } from "react-redux";
-import { useEffect } from "react";
 import Form from "./Form";
 import TaskList from "./TaskList";
 import Buttons from "./Buttons";
 import Section from "../../common/Section";
 import Header from "../../common/Header";
 import Container from "../../common/Container";
-import { selectTasksState } from "./tasksSlice";
 
 function Tasks() {
-  const { tasks, hideDone } = useSelector(selectTasksState);
-
-  useEffect(() => {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-    localStorage.setItem("hideDone", JSON.stringify(hideDone));
-  }, [tasks, hideDone]);
-
   return (
     <Container>
       <Header title="Lista zadań" />
