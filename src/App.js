1499aaeefe72df5efe76c2007744a0471c7f7993
Krystian Greblowski <krystian.greblowski@gmail.com>
2023-11-11 19:42:03 +0100

    Navigation appearance improved

--- src/App.js
+++ src/App.js
@@ -3,25 +3,22 @@ import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import TasksPage from "./features/tasks/TasksPage";
 import TaskPage from "./features/tasks/TaskPage";
 import AuthorPage from "./features/author/AuthorPage";
-import { StyledNavLink } from "./styled";
+import { StyledNavLink, StyledList, TasksItem, AuthorItem } from "./styled";
 
 const App = () => (
   <HashRouter>
     <nav>
-      <ul>
-        <li>
-          <StyledNavLink to="/zadania">
-            Zadania
-          </StyledNavLink>
-        </li>
-        <li>
-          <StyledNavLink to="/autor">
-            O autorze
-          </StyledNavLink>
-        </li>
-      </ul>
+      <StyledList>
+        <TasksItem>
+          <StyledNavLink to="/zadania">Zadania</StyledNavLink>
+        </TasksItem>
+        <AuthorItem>
+          <StyledNavLink to="/autor">O autorze</StyledNavLink>
+        </AuthorItem>
+      </StyledList>
+
       <Switch>
-      <Route path="/zadania/:id">
+        <Route path="/zadania/:id">
           <TaskPage />
         </Route>
         <Route path="/zadania">
--- src/styled.js
+++ src/styled.js
@@ -1,8 +1,34 @@
 import styled from "styled-components";
 import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 
+export const StyledList = styled.ul`
+  list-style-type: none;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 30px;
+  background-color: teal;
+  padding: 18px;
+  margin-top: 0;
+  font-size: 14px;
+`;
+
+export const TasksItem = styled.li`
+  justify-self: end;
+`;
+
+export const AuthorItem = styled.li`
+  justify-self: start;
+`;
+
 export const StyledNavLink = styled(NavLink)`
+  text-decoration: none;
+  color: white;
+
   &.active {
-    color: red;
+    font-weight: bold;
+  }
+
+  &:hover {
+    filter: brightness(90%);
   }
 `;
