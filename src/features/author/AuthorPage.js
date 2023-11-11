f60e60fedf15258dfde1918d617cd47dd491a1b2
Krystian Greblowski <krystian.greblowski@gmail.com>
2023-11-11 15:42:15 +0100

    react router added

---
+++ src/features/author/AuthorPage.js
@@ -0,0 +1,13 @@
+import Container from "../../common/Container";
+import Header from "../../common/Header";
+import Section from "../../common/Section";
+
+const Author = () => (
+  <Container>
+    <Header title="O autorze" />
+
+    <Section title="Krystian Gręblowski" body={<>naanana</>} />
+  </Container>
+);
+
+export default Author;
---
+++ src/features/tasks/Input/index.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+  border: 1px solid #ccc;
+  padding: 10px;
+`;
+
+export default Input;
