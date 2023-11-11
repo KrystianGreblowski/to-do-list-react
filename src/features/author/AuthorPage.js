400723be0264afa1675e16d28db430192d1a629d
Krystian Greblowski <krystian.greblowski@gmail.com>
2023-11-11 23:21:01 +0100

    information about me added

--- src/features/author/AuthorPage.js
+++ src/features/author/AuthorPage.js
@@ -1,11 +1,29 @@
 import Container from "../../common/Container";
 import Header from "../../common/Header";
 import Section from "../../common/Section";
+import { AboutMe } from "./styled";
 
 const Author = () => (
   <Container>
     <Header title="O autorze" />
-    <Section title="Krystian Gręblowski" body={<>naanana</>} />
+    <Section
+      title="Krystian Gręblowski"
+      body={
+        <>
+          <AboutMe>
+            Mam 29 lat i rok temu zainteresowałem się Frontendem. Pochodzę z
+            małej miejscowości na południu Polski. Zawsze jednak ciągnęło mnie
+            do dużych miast i w pewnym momencie swojego życia postanowiłem, że
+            zamieszkam w jednym z nich. I tak oto wylądowalem w Warszawie.
+          </AboutMe>
+          <AboutMe>
+            Interesuję się również sportem, szczególnie lubię pojawiać się na
+            siłowni. Natomiast w wolnym czasie chętnie przeczytam dobrą książkę lub
+            obejrzę ciekawy film.
+          </AboutMe>
+        </>
+      }
+    />
   </Container>
 );
 
